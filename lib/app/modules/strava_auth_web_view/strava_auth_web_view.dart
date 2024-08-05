96d158c1a4a168401017f10e5087d1f19b97085a
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2024-08-05 14:59:08 +0530

    web view optimised

--- lib/app/modules/strava_auth_web_view/strava_auth_web_view.dart
+++ lib/app/modules/strava_auth_web_view/strava_auth_web_view.dart
@@ -3,25 +3,32 @@ import 'package:flutter/material.dart';
 import '../../app_api_services/api_end_point.dart';
 import '../../common_widgets/common_web_view_widget.dart';
 
-class StravaAuthWebView extends StatelessWidget {
+class StravaAuthWebView extends StatefulWidget {
   final String userId;
   const StravaAuthWebView({super.key, required this.userId});
 
+  @override
+  State<StravaAuthWebView> createState() => _StravaAuthWebViewState();
+}
+
+class _StravaAuthWebViewState extends State<StravaAuthWebView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CommonWebViewWidget(
         webViewUrl: ApiEndPoint.stravaAuthUrl(
-          userId,
+          widget.userId,
         ),
-        webViewBackUrl: "https://fitbe-dev.skill-mine.com/strava/test/code/$userId?state=&code=",
+        webViewBackUrl: "https://fitbe-dev.skill-mine.com/strava/test/code/${widget.userId}?state=&code=",
         onPageFinished: (url) {
-          if (url.contains('https://fitbe-dev.skill-mine.com/strava/test/code/$userId?state=&code=')) {
-            Navigator.pushNamedAndRemoveUntil(
-              context,
-              "/homeScreen",
-              (Route<dynamic> route) => false,
-            );
+          if (url.contains('https://fitbe-dev.skill-mine.com/strava/test/code/${widget.userId}?state=&code=')) {
+            if(mounted) {
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                "/homeScreen",
+                    (Route<dynamic> route) => false,
+              );
+            }
           }
         },
       ),
